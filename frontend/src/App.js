739024aa09192bb7f08f400420afc0e72f31bcf8
anupriyalathey <anupriyalathey@gmail.com>
2023-11-20 22:47:10 +0530

    made signin and signup pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,26 @@
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 import SalesPredictionForm from './components/SalesPredictionForm';
+import { ChakraProvider } from '@chakra-ui/react'
+import Signin from './components/Signin';
+import Signup from './components/Signup';
+import Home from './components/Home';
 
 function App() {
   return (
+    <ChakraProvider>
     <div className="App">
-    <SalesPredictionForm />
+      <Router>
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/signin' element={<Signin />} />
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/sales' element={<SalesPredictionForm />} />
+        </Routes>
+      </Router>
   </div>
+  </ChakraProvider>
   );
 }
 
---
+++ frontend/src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { Link } from "react-router-dom";
+// import PropTypes from "prop-types";
+import { Text} from "@chakra-ui/react";
+
+export default function Home() {
+  return (
+    <Link to="/signin">Sign In</Link>
+  )
+  }
